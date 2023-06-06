2bf6ebce8f83f02261db3b4e89635a26eadb7102
MauricioAires <mauricioaires015@gmail.com>
2023-06-06 15:20:05 -0300

    feat(document) config next-reveal

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,13 +3,22 @@ import Document, { Html, Main, NextScript, Head } from 'next/document'
 export default class MyDocument extends Document {
   render(): JSX.Element {
     return (
-      <Html lang="pt-BR">
+      <Html lang="pt-BR" className="sr">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Archivo:wght@400;500;700&display=swap"
             rel="stylesheet"
+          />
+          <style
+            dangerouslySetInnerHTML={{
+              __html: {
+                'html.sr .load-hidden': {
+                  visibility: 'hidden'
+                }
+              }
+            }}
           />
         </Head>
 
